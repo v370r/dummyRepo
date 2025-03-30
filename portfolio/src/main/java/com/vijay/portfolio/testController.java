0ce58195d80d29b95b586d2f8e73a056eb913cba
v370r <studyrockers5@gmail.com>
2025-03-29 19:22:42 -0600

    first commit

---
+++ portfolio/src/main/java/com/vijay/portfolio/testController.java
@@ -0,0 +1,12 @@
+package com.vijay.portfolio;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin("*")
+@RequestMapping()
+public class testController {
+
+}
